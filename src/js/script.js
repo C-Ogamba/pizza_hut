4bcab5e3c280f7c37afe8bc932a8c47b43d605bd
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-03-21 10:22:21 +0300

    add functionality to select button

--- src/js/script.js
+++ src/js/script.js
@@ -5,33 +5,41 @@ const closeCartBtn = document.querySelector(".close-cart");
 const clearCartBtn = document.querySelector(".clear-cart");
 const cartDOM = document.querySelector(".cart");
 const cartOverlay = document.querySelector(".cart-overlay");
-const cartItems= document.querySelector(".cart-items");
+const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".product-center");
+const getOrder = document.querySelector(".data");
+const orderNumber = document.querySelector(".number");
+const pizzaSize = document.querySelector(".size");
+const pizzaType = document.querySelector(".type");
+const pizzaToppings = document.querySelector(".toppings");
+const pizzaTotal = document.querySelector(".total");
+const getTotal = document.querySelector(".order");
+getTotal.addEventListener("click", function () {
+  buildPizza();
+  getOrder.classList.add("random");
+});
+function buildPizza() {
+  let pizzaGenerator = document.querySelector("#pizza-size").value;
+  let typeToppings = document.querySelector("#pizza-toppings").value;
+  let typeCrust = document.querySelector("#pizza-crust").value;
+}
 
-let cart= [];
+let cart = [];
 
 // getting the products
- class Products{
-
- }
+class Products {
+  getProducts() {
+    fetch("prodc");
+  }
+}
 //  dispaly products
-
-class UI{
-
-}
-
-class Storage{
-
-}
+class UI {}
+// local storage
+class Storage {}
 
 document.addEventListener("DOMContentLoaded", () => {
-    const ui = new UI();
-    const products = new Products();
-})
-
-
-
-
-
+  const ui = new UI();
+  const products = new Products();
+});
